76b55ed563aecb56089e965057d7ac133255e0d8
Antonych50 <Antonych50@gmail.com>
2025-03-04 10:01:26 +0300

    1
    Calculate average for each subject and student

---
+++ main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+
+students = ['Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Попов', 'Смирнов', 'Васильев', 'Михайлов', 'Новиков', 'Федоров']
+subjects = ['Информатика', 'Математика', 'Физика', 'Химия', 'История']
+data = {'Студент': students}
+for subject in subjects:
+    data[subject] = [random.choices([2, 3, 4, 5], k=random.randint(1, 10)) for _ in range(len(students))]
+
+df = pd.DataFrame(data)
+#print(df)
+#print(df.head())
+print(df.loc[0])
+summa = 0
+length = 0
+total_summa = []
+total_length = []
+with open('result.txt', 'w', encoding='utf-8') as f:
+    for i in range(len(students)):
+        #if i>0:
+            #break
+        for j in range(len(subjects)):
+            summa += np.sum(df.loc[i][subjects[j]])
+            length += len(df.loc[i][subjects[j]])
+            print(f'{df.loc[i]["Студент"]} {subjects[j]} {np.round(summa/length,2)}') #  {summa} {length} {summa/length}')
+            f.write(f'{df.loc[i]["Студент"]} {subject} {np.round(summa/length,2)}\n')
+            summa = 0
+            length = 0
+
+
